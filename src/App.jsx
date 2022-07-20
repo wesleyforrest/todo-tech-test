18dc5333aee6a3bcd771ea7396db7bf376ffe21f
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-07-20 16:50:50 +0100

    Added code to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import "./Styles/App.scss";
 import Header from "./Containers/Header/Header";
 import Main from "./Containers/Main/Main";
+import { useState } from "react";
 
 function App() {
+  const [todoArray, set todoArray] = useState([])
+  const [showTodo, setshowTodo] = useState(false)
+
+const handleInput = (event) => {
+  event.preventDefault();
+  const todoItem = event.target[0].value
+  if(todoItem.length > 0) {
+    const newArr
+  }
+}
+
+
   return (
     <div className="App">
       <Header />
